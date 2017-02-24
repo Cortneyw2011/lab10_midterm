27d8e94542f9e04994dfe345aa9dac42810b328c
Ashton Albertson <ashtonalbertson@gmail.com>
2017-02-23 19:06:39 -0500

    added chair1 fade

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 $(".form").hide()
-$( "#chair1" ).on( "click" , function(){
-	$( ".form" ).show()
+$( "#chair1" ).click(function(){
+	$( ".form" ).show();
+	$("#chair1").fadeTo("slow", 0.75);
 	});
 });
