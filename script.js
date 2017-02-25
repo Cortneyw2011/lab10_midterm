bedeb8e7b98d582334220cfee9877a558f29202a
Jay Fincher <fincher_jay@yahoo.com>
2017-02-25 09:21:15 -0500

    Updated jquery to save form information

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ $("img").click(function(){
  	$(".form").show();
 	$(this).fadeTo("fast", 0.3);
 });
+
+// Form Array   >>>>>>>>>>>>>>>>>>>
+
+$( "form" ).submit(function( event ) {
+  console.log( $( ".form" ).serializeArray() );
+  event.preventDefault();
+	});
 });
